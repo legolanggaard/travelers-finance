cfd72c117dcce06a23fedfdf3f85ceee675a1cc4
Lars Juul Langgaard <dkLaJuLa@a-flyfq13.corp.lego.com>
2023-02-03 14:45:55 +0100

    adding rabbit mq

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ctx, MessagePattern, Payload, RmqContext } from "@nestjs/microservices";
 
 @Controller()
 export class AppController {
@@ -8,5 +9,16 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('rabbit-mq-producer')
+  public async execute(
+    @Payload() data: any,
+    @Ctx() context: RmqContext
+  ) {
+    const channel = context.getChannelRef();
+    const orginalMessage = context.getMessage();
+    console.log('data', data);
+    channel.ack(orginalMessage);
   }
 }
